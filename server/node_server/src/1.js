bda582dea18a710d7d1a92e4e845017517aebdcc
gotSomeCool <wsljw222@gmail.com>
2019-12-29 17:07:14 +0800

    1

---
+++ server/node_server/src/1.js
@@ -0,0 +1,12 @@
+const sql = require('mssql');
+
+async function getDB() {
+  const config = {
+    user: 'SA',
+    password: '<A12345678@Passw0rd>',
+    server:'106.14.224.131',
+    database:'gabiss_db'
+  }
+  
+}
+getDB();
